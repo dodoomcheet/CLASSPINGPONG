d1d2d610d8e8cd4f6d3cf28bac76c7ce4ddbcca5
dodoomcheet <53454864+dodoomcheet@users.noreply.github.com>
2024-10-07 21:45:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 UploadedPosts = [1,2,3];
 let NumberOfUploadedPosts = UploadedPosts.length;
 console.log(UploadedPosts+" "+NumberOfUploadedPosts);
+let posts = []
 
 function check_enter() {
   if (event.keyCode == 13) {
     event.returnValue=false;
   }
+}
+
+function newpost(text) {
+  posts.push("text");
+  alert.(posts[posts.length-1]);
 }
